6d30a67af79ae2d5da211f147f321542c275e807
Dhrumil Choksi <41354644+choksidhrumil2000@users.noreply.github.com>
2020-04-30 17:45:13 +0530

    Add files via upload

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -6,4 +6,6 @@ app_name = 'user_profile'
 
 urlpatterns = [
     url(r'^profile/$',views.user_profile,name="profile"),
+    path('edit/',views.edit_profile,name = "Edit_profile"),
+    path('change_password/',views.change_password,name = "change_password"),
 ]
